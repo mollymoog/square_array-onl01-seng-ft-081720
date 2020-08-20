968a862301eb110ccdca525bdfb718940df5b944
molly mk <mmkover@gmail.com>
2020-08-20 17:45:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array.each do |number|
     new_numbers = number ** 2
     puts new_numbers
-    Array.new(new_numbers) = []
+    Array.new (new_numbers) = []
     
   end
   
