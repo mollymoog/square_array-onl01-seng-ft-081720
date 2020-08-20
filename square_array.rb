8e097b18813b25b1f9de085e54a093ea96a34005
molly mk <mmkover@gmail.com>
2020-08-20 17:53:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 require 'pry'
 def square_array(array)
-  array.each do |number|
+  new_array = array.each do |number|
     new_numbers = number ** 2
-    Array.new (3, new_numbers)
-    puts Array.new
-    binding.pry
+
   end
   
 end
